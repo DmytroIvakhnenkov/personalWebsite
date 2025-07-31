aeb40d100829c3f128c466cf028c3f247fc68e6e
Dmytro Ivakhnenkov <dmytro.ivakhnenkov@gmail.com>
2025-07-31 19:39:22 +0800

    chore: update worked hours for 2025-07-31 19:00

--- cronJob.py
+++ cronJob.py
@@ -1,11 +1,14 @@
 import subprocess
+from datetime import datetime
 
 repo_path = "/home/dmytro/BigDmytro/personalWebsite"
 
 # Run the csvProcessing script
 subprocess.run(["python3", "csvProcessing.py"], cwd=repo_path)
 
+now = datetime.now()
+timestamp = now.strftime("%Y-%m-%d %H:00")
 # Git operations
 subprocess.run(["git", "add", "."], cwd=repo_path)
-subprocess.run(["git", "commit", "-m", "Hourly update"], cwd=repo_path)
+subprocess.run(["git", "commit", "-m", f"chore: update worked hours for {timestamp}"], cwd=repo_path)
 subprocess.run(["git", "push"], cwd=repo_path)
